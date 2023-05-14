bf555c533f89169034330326385988147010b77d
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-05-14 11:18:50 -0400

    lcp

--- src/longest_common_prefix.rs
+++ src/longest_common_prefix.rs
@@ -1,5 +1,35 @@
 fn longest_common_prefix(strs: &[&str]) -> String {
 
+    let mut prefix = String::new();
+
+    if strs.len() == 0 {
+        return prefix;
+    }
+
+    let mut i = 0;
+    let mut j = 0;
+
+    for c in strs[0].chars() {
+        for s in strs {
+
+            if s.len() <= i {
+                return prefix;
+            }
+
+            if s.chars().nth(i).unwrap() != c {
+                return prefix;
+            }
+
+
+        }
+
+    }
+
+
+
+
+
+
 
 }
 
